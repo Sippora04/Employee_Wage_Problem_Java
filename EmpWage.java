e70d8a84e30cb1324e4a5d850f2f7726af3df3f7
Sippora04 <sippora.toppo95@gmail.com>
2020-06-11 23:40:04 +0530

    REFACTOR: change into Switch Case

--- EmpWage.java
+++ EmpWage.java
@@ -1,7 +1,27 @@
-class EmpWage
-{
-    public static void main (String args[])
-    {
-        System.out.println("Welcome to Employee Wage Computation Problem");
-    }
+public class EmpWage
+{  
+   public static final int EMP_RATE_PER_HOUR = 20;    
+   public static final int IS_PART_TIME = 1;
+   public static final int IS_FULL_TIME = 2; 
+   public static void  main(String args[])
+   {
+    int empHrs=0;
+    int empWage=0;
+    System.out.println("Welcome to Employee Wage Computation/Problem");
+        int empCheck = (int) Math.floor(Math.random() * 10)%3;
+        switch (empCheck)
+        {
+         case IS_PART_TIME: System.out.println("Partime");
+                            empHrs = 4;
+                            break;
+        
+         case IS_FULL_TIME: System.out.println("Fulltime");
+                            empHrs = 8;
+                            break;
+         default: System.out.println("Absent");
+                  empHrs = 0;
+        }
+        empWage = empHrs * EMP_RATE_PER_HOUR;
+        System.out.println("Employee Wage: " + empWage );
+   }
 }
