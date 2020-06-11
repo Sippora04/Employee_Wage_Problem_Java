5d985a285aac2aec6b88895dac1f4f3593c40ad4
Sippora04 <sippora.toppo95@gmail.com>
2020-06-11 22:36:53 +0530

    Add: Employee Daily Wage Calculation

--- EmpWage.java
+++ EmpWage.java
@@ -1,7 +1,27 @@
-class EmpWage
-{
-    public static void main (String args[])
-    {
-        System.out.println("Welcome to Employee Wage Computation Problem");
-    }
+import java.util.Random;
+public class EmpWage
+{  
+   static int dailyWage = 0;
+   static int EMP_RATE_PER_HOUR = 20;
+   static int FULL_TIME_HOURS = 8;
+   static int FULL_TIME_ABSENT = 0;
+   public static void main(String args[])
+   {
+      System.out.println("Welcome to Employee Wage Computation/Problem");
+      Random random = new Random();
+        int empCheck = random.nextInt()%3;
+        if(empCheck == 0 )
+        {
+            System.out.println("Employee is Absent");
+            dailyWage = FULL_TIME_ABSENT * EMP_RATE_PER_HOUR;
+            System.out.println("Employee dailyWage is "+dailyWage);
+            
+        }
+        else
+        {
+            System.out.println("Employee is Present");
+            dailyWage = FULL_TIME_HOURS * EMP_RATE_PER_HOUR;
+            System.out.println("Employee dailyWage is "+dailyWage);
+        }
+   }
 }
